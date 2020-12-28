6f3f88b926c2b8c714ad39a2570aba31bfb2d66e
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-28 22:56:26 +0400

    fix coments

--- callback.js
+++ callback.js
@@ -3,19 +3,22 @@ let addName = document.querySelector(".name");
 let persons = ["Aro", "Gugo"];
 
 function htmlMessages() {
-  let htmlMessage = "";
-
-  persons.forEach((person) => {
-    htmlMessage += `
+  setTimeout(() => {
+    let htmlMessage = "";
+    persons.forEach((person) => {
+      htmlMessage += `
       <li>${person}</li>
      `;
-  });
-  addName.innerHTML = htmlMessage;
+    });
+    addName.innerHTML = htmlMessage;
+  }, 2000);
 }
 
-function foo2() {
-  persons.push("asasdas");
+function foo2(name, callback) {
+  setTimeout(() => {
+    persons.push(name);
+    callback();
+  }, 4000);
 }
 
-htmlMessages();
-setTimeout(foo2, 4000);
+foo2("Edik", htmlMessages);

47f2c38cf3040447fc1c9aba79472f3f523cf697
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-28 20:09:15 +0400

    fixed the code

---
+++ index.js
@@ -0,0 +1,21 @@
+let persons = ["Aro", "Gugo"];
+
+function htmlMessages(callback) {
+  callback();
+
+  persons.forEach((person) => {
+    let item = document.createElement("li");
+    item.innerText = person;
+    document.body.append(item);
+  });
+}
+
+function foo2() {
+  persons.push("asasdas");
+
+  console.log(persons);
+}
+
+htmlMessages(function () {
+  setTimeout(foo2, 4000);
+});
